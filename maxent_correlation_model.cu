89a995238373a2c33f0a166656090d65fc5cbb2a
Brad Theilman <btheilma@ucsd.edu>
2016-06-02 11:31:23 -0700

    Works with first order kernel

--- maxent_correlation_model.cu
+++ maxent_correlation_model.cu
@@ -18,28 +18,36 @@ __global__ void gibbs_sample(curandState *state, float* alpha, float* beta, floa
 	int blk = blockIdx.x;
 	int i, j;
 	int cell, samp;
-	float df, bdif, p1;
+	float df=0.0;
+	float bdif=0.0;
+	float p1=0.0;
 	for(samp=0; samp < nsamps; samp++)
 	{
 		int sampstart = blk*N*nsamps + samp*N;
 		/* need to include nsweeps */
 		for(cell=0; cell < N; cell++)
 		{
-
+			bdif=0.0;
+			df = 0.0;
+			p1=0.0;
 			for(j=0; j<=cell-1; j++)
 			{
-				bdif += beta[j*N-j*(j+1)/2 + cell];
+				bdif += 2*beta[j*N-j*(j+1)/2 + cell];
 			}
-			for(j=cell+1; j<=N-1; j++)
+			/*for(j=cell+1; j<=N-1; j++)
 			{
 				bdif += beta[cell*N - cell*(cell+1)/2 +j];
-			}
+			}*/
 			
-			df = -1.0*alpha[cell] - bdif;
+			df = -1.0*alpha[cell] - 0.0*bdif;
 			p1 =expf(df)/(1+expf(df));
 			if(curand_uniform(&state[blk]) < p1)
 			{
 				samples[sampstart + cell] = 1.0;
+			}
+			else
+			{
+				samples[sampstart + cell] = 0.0;
 			}
 		}
 	}
@@ -53,7 +61,7 @@ __global__ void compute_sample_mean(float* samples, float* sample_mean, int nsam
 	{
 		sample_mean[cell] += samples[cell + i*N];
 	}
-	sample_mean[cell] = sample_mean[cell] / nsamps;
+	/*sample_mean[cell] = sample_mean[cell] / nsamps;*/
 }
 
 __global__ void compute_sample_covariance(float* samples, float* sample_covariance, int N)
@@ -68,6 +76,29 @@ __global__ void compute_sample_covariance(float* samples, float* sample_covarian
 __global__ void update_parameter_estimates(float* alpha, float* beta, float* sample_mean, float* sample_covariance, int N)
 {
 
+}
+
+
+int write_paths_to_csv(char* fname, float* paths, int n_paths, int nt, char* mode)
+{
+    
+    FILE *outfile;
+    outfile = fopen(fname, mode);
+    
+    int i, j;
+    if(outfile != NULL)
+    {
+        for(i = 0; i<n_paths; i++)
+        {
+            for(j = 0; j<nt; j++)
+            {
+                fprintf(outfile, "%f,", paths[i*nt + j]);
+            }
+            fprintf(outfile, "\n");
+        }
+    }
+    fclose(outfile);
+    return 0;
 }
 
 int main()
@@ -88,6 +119,7 @@ int main()
 	float *alpha_res = (float*)malloc(mean_size);
 	float *beta_res = (float*)malloc(cov_size);
 	float *sample_mean_res = (float*)malloc(mean_size);
+	float *samples_res = (float*)malloc(samples_size);
 
 	/* Allocate device memory */
 	float *sample_mean;
@@ -109,11 +141,12 @@ int main()
 	int i;
 	for(i=0; i<N; i++)
 	{
-		alpha_res[i] = 2*rand()-1;
+		alpha_res[i] = (2*((float)rand()/(float)RAND_MAX) - 1);
+		/*printf("%f\n", alpha_res[i]);*/
 	}
 	for(i=0; i<N*(N-1)/2; i++)
 	{
-		beta_res[i]=2*rand()-1;
+		beta_res[i]= 0.0*(2*((float)rand()/(float)RAND_MAX) - 1);
 	}
 
 	/* copy initial conditions over to device */
@@ -124,18 +157,20 @@ int main()
 	printf("Sampling...\n");
 	init_rand<<<nblocks, 1>>>(d_state);
 	gibbs_sample<<<nblocks, 1>>>(d_state, alpha, beta, samples, N, samps_per_block, nsweeps);
-	printf("Finished.  nsamps=%d", nsamps_tot);
-	printf("Computing Sample mean...");
+	printf("Finished.  nsamps=%d\n", nsamps_tot);
+	printf("Computing Sample mean...\n");
 	compute_sample_mean<<<N, 1>>>(samples, sample_mean, nsamps_tot, N);
 
 	/*copy back sample mean*/
 	cudaMemcpy(sample_mean_res, sample_mean, mean_size, cudaMemcpyDeviceToHost);
+	cudaMemcpy(samples_res, samples, samples_size, cudaMemcpyDeviceToHost);
 
 	/* Display*/
 	for(i=0; i<N; i++)
 	{
-		printf("%f\n", sample_mean_res[i]);
+		printf("%2.1f\n", sample_mean_res[i]);
 	}
+	write_paths_to_csv("maxent_samples.csv", samples_res, nsamps_tot, N, "w");
 
 
 	/* free memory */
